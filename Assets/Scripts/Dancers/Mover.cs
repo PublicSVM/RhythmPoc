d86d0c289d1a6cdc09b356947234eeb87f99d6b2
PublicSVM <contact.psvm@gmail.com>
2016-07-31 14:00:52 +0200

    Implementation of mover is closer to it's original intent
    Should probably make use of Update instead of FixedUpdate and using
    force/speed, but that can probably wait

--- Assets/Scripts/Dancers/Mover.cs
+++ Assets/Scripts/Dancers/Mover.cs
@@ -3,6 +3,8 @@ using Assets.Scripts.Events;
 
 public class Mover : MonoBehaviour
 {
+    private const float MAX_FADE = 75f;
+
     public Conductor Conductor;
 
     public GameObject From;
@@ -14,6 +16,14 @@ public class Mover : MonoBehaviour
     public int ToMeasure;
 
     private AudioSource _audioSource;
+    private float _fade;
+    private Vector3 _start;
+    private Vector3 _end;
+
+    public Mover()
+    {
+        _fade = -1;
+    }
 
     void Start()
     {
@@ -23,28 +33,33 @@ public class Mover : MonoBehaviour
         Conductor.Subscribe(OnBeat);
     }
 
-    private void OnBeat(int beat, int measure)
+    void FixedUpdate()
     {
-        if (measure % 8 == FromMeasure)
+        if (_fade > -1)
         {
-            Move(From, To, beat);
-            _audioSource.PlayOneShot(FromSound);
-        }
-        else if (measure % 8 == ToMeasure)
-        {
-            Move(To, From, beat);
-            _audioSource.PlayOneShot(ToSound);
+            float x = _start.x + (_end.x - _start.x) * ((MAX_FADE - _fade) / MAX_FADE);
+            float y = _start.y + (_end.y - _start.y) * ((MAX_FADE - _fade) / MAX_FADE);
+            float z = _start.z + (_end.z - _start.z) * ((MAX_FADE - _fade) / MAX_FADE);
+            transform.position = new Vector3(x, y, z);
+            _fade--;
         }
     }
 
-    private void Move(GameObject startObject, GameObject endObject, int beat)
+    private void OnBeat(int beat, int measure)
     {
-        var start = startObject.transform.position;
-        var end = endObject.transform.position;
-        float x = end.x + (start.x - end.x) * ((3f - beat) / 4f);
-        float y = end.y + (start.y - end.y) * ((3f - beat) / 4f);
-        float z = end.z + (start.z - end.z) * ((3f - beat) / 4f);
-
-        transform.position = new Vector3(x, y, z);
+        if (measure % 8 == FromMeasure && beat == 0)
+        {
+            _start = From.transform.position;
+            _end = To.transform.position;
+            _audioSource.PlayOneShot(FromSound);
+            _fade = MAX_FADE;
+        }
+        else if (measure % 8 == ToMeasure && beat == 0)
+        {
+            _start = To.transform.position;
+            _end = From.transform.position;
+            _audioSource.PlayOneShot(ToSound);
+            _fade = MAX_FADE;
+        }
     }
 }
