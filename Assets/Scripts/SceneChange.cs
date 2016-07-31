5a9177bdfc2534502c1e6a44dec2dbddf70630e1
PublicSVM <contact.psvm@gmail.com>
2016-07-31 12:19:27 +0200

    Scene changes when pressing space

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -4,21 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    public Conductor Conductor;
+    public string ActivationKey;
     public string SceneName;
-    public int ChangeOnMeasure;
-    public int ChangeOnBeat;
 
-    // TODO Change to using a string (key) and string (next scene) instead of using the beat
-
-    void Start()
+    void Update()
     {
-        Conductor.Subscribe(ChangeLevel);
-    }
-
-    void ChangeLevel(int beat, int measure)
-    {
-        if (measure == ChangeOnMeasure && beat == ChangeOnBeat)
+        if (Input.GetKeyDown(ActivationKey))
         {
             SceneManager.LoadScene(SceneName);
         }
