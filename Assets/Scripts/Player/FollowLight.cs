da71c2f26effd0aa22db377d2e0e20f9a60abfb3
PublicSVM <contact.psvm@gmail.com>
2016-08-01 19:40:40 +0200

    Added player w/ controls and trail

---
+++ Assets/Scripts/Player/FollowLight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    class FollowLight : MonoBehaviour
+    {
+        public Player Player;
+
+        private Transform _transform;
+
+        void Start()
+        {
+            _transform = GetComponent<Transform>();
+        }
+
+        void Update()
+        {
+            var x = 90 + Player.Velocity.magnitude * 3;
+            var y = Vector3.Angle(Vector3.forward, Player.Velocity);
+            if (Player.Velocity.x < 0) y = -y;
+            _transform.localRotation = Quaternion.Euler(x, y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    class Player : MonoBehaviour
+    {
+        public float Force = 20;
+        public float SpeedLimit = 10;
+
+        public Vector3 Velocity
+        {
+            get { return _rigidbody.velocity; }
+            private set { _rigidbody.velocity = value; }
+        }
+
+        private Vector3 _vertical;
+        private Vector3 _horizontal;
+        private Vector3 _stop;
+
+        private Rigidbody _rigidbody;
+
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+
+            _vertical = new Vector3(0, 0, Force);
+            _horizontal = new Vector3(Force, 0, 0);
+            _stop = new Vector3(0, 0, 0);
+        }
+
+        void Update()
+        {
+            HandleInput();
+        }
+
+        private void HandleInput()
+        {
+            var vertical = Input.GetAxis("Vertical");
+            _rigidbody.AddForce(_vertical * vertical);
+
+            var horizontal = Input.GetAxis("Horizontal");
+            _rigidbody.AddForce(_horizontal * horizontal);
+
+            if (Mathf.Abs(vertical) < 0.1 && Mathf.Abs(horizontal) < 0.1)
+            {
+                if (Velocity.magnitude > 0.2)
+                    Velocity *= 0.8f;
+                else
+                    Velocity = _stop;
+            }
+
+            if (Velocity.magnitude > SpeedLimit)
+            {
+                Velocity = Velocity.normalized * SpeedLimit;
+            }
+        }
+    }
+}
